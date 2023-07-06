46e9582b02011076e99e5e65f979010b0c71f39d
Vis <vt201916384@gmail.com>
2023-07-05 20:54:01 -0400

    setup video processing endpoint

--- api/video/app.py
+++ api/video/app.py
@@ -1,1 +1,23 @@
 from fastapi import FastAPI
+from transcribe import transcribe
+from chain import chain
+from pydantic import BaseModel
+from typing import List
+
+
+app = FastAPI()
+
+
+class Req(BaseModel):
+    file_name: str
+    # summary: str
+
+class Res(BaseModel):
+    summary: List[str]
+
+
+@app.post("/video", status_code=200, response_model=Res)
+def get_video_summary(req: Req) -> Res:
+    transcription = transcribe(req.file_name)
+    summary = chain.run(input_text=transcription)
+    return Res(summary)
