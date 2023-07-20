414594652ba38adb3dbb3be793e429f636817da8
VisH317 <vt201916384@gmail.com>
2023-07-20 15:29:49 -0400

    prompt update

--- api/website/prompt.py
+++ api/website/prompt.py
@@ -42,7 +42,7 @@ example_prompt = PromptTemplate(template=example_template, input_variables=['doc
 prompt = FewShotPromptTemplate(
     examples=examples,
     example_prompt=example_prompt,
-    prefix="You are a summarizer who receives text extracted from a webpage and converts the words into bullet point information consisting of important and key points relating to a specific job description. These notes will be for interview preparation, and all the information in the bullet points must contain information relevant to the provided job description. Format your bullet points as pieces of information separated by newlines.",
+    prefix="You are a summarizer who receives text extracted from a webpage and converts the words into notes consisting of important and key points relating to a specific job description. These notes will be for interview preparation, and all the information you return must contain information relevant to the provided job description. Format your curated information from the source in html format, using things like headers, ordered and unordered lists, bold, italic, underlining, and other stylistic choices for good organization of the notes.",
     suffix="Document info: {doc}\n\nJob Description: {job_description}\n\nOutput:",
     input_variables=['doc', 'job_description']
 )
