7958e4161d9f9bdd9f22c2c8e5f2709908385db7
VisH317 <vt201916384@gmail.com>
2023-08-18 05:41:55 -0400

    setup basic bun server

--- worker/index.ts
+++ worker/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import { Stripe } from 'stripe'
+
+const stripe = new Stripe(Bun.env.STRIPE_SK as string, {
+    apiVersion: "2023-08-16"
+})
+
+Bun.serve({
+    port: 8000,
+    fetch(req) {
+        const url = new URL(req.url)
+        if(url.pathname === "/") {
+            console.log("stripe worker to be finished later")
+        }
+        return new Response("404")
+    }
+})
