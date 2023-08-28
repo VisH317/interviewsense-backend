0c9c7a3f5f8a0f0bda159c360bc2a79f3f1db0c3
VisH317 <vt201916384@gmail.com>
2023-08-28 16:53:48 -0400

    update loading strategy

--- api/website/scraper.py
+++ api/website/scraper.py
@@ -5,17 +5,24 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-gpu')
+
+capa = DesiredCapabilities.CHROME
+capa["pageLoadStrategy"] = "eager"
+
 
 service = Service(ChromeDriverManager(driver_version='114.0.5735.90').install())
 
 def getHTML(url: str):
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(service=service, options=chrome_options, desired_capabilities=capa)
     fp = driver.get(url)
     print("fp: ", )
     html = driver.page_source
